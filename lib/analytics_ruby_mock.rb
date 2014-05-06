82540de0d6585b53dc1f87af36a0f8db47711e63
Khosrow Afroozeh <khosrow@doxter.de>
2014-05-06 11:29:00 +0200

    Event can be accessed with ['event'], too. Added .last_call and .last_event

--- lib/analytics_ruby_mock.rb
+++ lib/analytics_ruby_mock.rb
@@ -13,7 +13,7 @@ module AnalyticsRuby
     end
 
     def tracked_events
-      (@track_calls || []).collect { |t| t[:event] }
+      (@track_calls || []).collect { |t| t[:event] || t['event'] }
     end
 
     #############################################################
@@ -52,6 +52,14 @@ module AnalyticsRuby
 
     def clear
       initialize
+    end
+
+    def last_event
+      tracked_events.last
+    end
+
+    def last_call
+      @track_calls.last
     end
 
     private
